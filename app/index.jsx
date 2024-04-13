7360b803ea4226923f482a70983bb654c0b52e2f
Sumit <jagtasumit668@gmail.com>
2024-04-13 19:29:37 +0530

    assets setup

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white text-red-200">
-      <Text className="text-3xl">This is my first app</Text>
+      <Text className="text-3xl font-pblack">This is my first app</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         go to profile
